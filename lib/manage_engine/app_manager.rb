db75fb83e47a3d1ecff6c72a838fe28d29d99797
Justin Karimi <jekhokie@gmail.com>
2013-06-14 11:44:39 -0500

    Update for Default Port
    Update the AppManager module to ensure a default port is always passed
    if the user does not specify one.

--- lib/manage_engine/app_manager.rb
+++ lib/manage_engine/app_manager.rb
@@ -3,11 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/app_manager/server')
 
 module ManageEngine
   module AppManager
+    DEFAULT_PORT = 9090
+
     class << self
       # Public: Initializes a new ManageEngine::AppManager::Instance.
       #
       # host    - Host on which the ApplicationsManager server is running.
-      # port    - Port on which the ApplicationsManager is listening.
+      # port    - Optional port on which the ApplicationsManager is listening (default = 9090).
       # ssl     - Optional boolean indicating whether the server is listening using http-secure (default = false).
       # options - Optional hash used to configure this ManageEngine::AppManager::Instance (default = nil).
       #
@@ -16,7 +18,7 @@ module ManageEngine
       #   ManageEngine::AppManager.new :host => 'http://myHost.internal.com', :port => 9090
       #
       # Returns a ManageEngine::AppManager::Instance.
-      def new(host, port, options = nil)
+      def new(host, port = DEFAULT_PORT, options = nil)
         @server = ManageEngine::AppManager::Server.new host, port, options
       end
     end
