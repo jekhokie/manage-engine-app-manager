b96e171f3aec535f67739b71cd23788500c77df4
Justin Karimi <jekhokie@gmail.com>
2013-06-14 09:23:40 -0500

    Add .new for AppManager
    Add specs and .new method to create a new AppManager instance.

--- lib/manage_engine/app_manager.rb
+++ lib/manage_engine/app_manager.rb
@@ -2,6 +2,22 @@ require "manage_engine/app_manager/version"
 
 module ManageEngine
   module AppManager
-    # Your code goes here...
+    class << self
+      # Public: Initializes a new ManageEngine::AppManager::Instance.
+      #
+      # host    - Host on which the ApplicationsManager server is running.
+      # port    - Port on which the ApplicationsManager is listening.
+      # ssl     - Optional boolean indicating whether the server is listening using http-secure (default = false).
+      # options - Optional hash used to configure this ManageEngine::AppManager::Instance (default = nil).
+      #
+      # Examples
+      #
+      #   ManageEngine::AppManager.new :host => 'http://myHost.internal.com', :port => 9090
+      #
+      # Returns a ManageEngine::AppManager::Instance.
+      def new(host, port, ssl = false, options = nil)
+        ManageEngine::AppManager::Instance.new(host, port, ssl, options)
+      end
+    end
   end
 end
--- spec/appmanager_api_spec.rb
+++ spec/appmanager_api_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe ManageEngine::AppManager do
+  subject { ManageEngine::AppManager }
+
+  it "can create a new instance" do
+    subject.should respond_to(:new)
+  end
 end
