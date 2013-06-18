6a1920331e46dfd238d031f21686f0f50b314664
Justin Karimi <jekhokie@gmail.com>
2013-06-18 07:09:35 -0500

    Update AppManager new Method Documentation
    Update the comments within the AppManager module to more specifically
    denote what parameters are required for the new method.

--- lib/manage_engine/app_manager.rb
+++ lib/manage_engine/app_manager.rb
@@ -1,5 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + '/app_manager/version')
-require File.expand_path(File.dirname(__FILE__) + '/app_manager/server')
+require_relative 'app_manager/version'
+require_relative 'app_manager/server'
+require_relative 'app_manager/api'
 
 module ManageEngine
   module AppManager
@@ -8,6 +9,7 @@ module ManageEngine
       #
       # host    - Host on which the ApplicationsManager server is running.
       # port    - Optional port on which the ApplicationsManager is listening (default = 9090).
+      # api_key - REST key used to communicate with the AppManager service.
       # options - Optional hash used to configure this ManageEngine::AppManager::Instance (default = nil).
       #             :api_version - Version of the AppManager API to use (drives the schema)
       #
@@ -16,6 +18,7 @@ module ManageEngine
       #   ManageEngine::AppManager.new :host => 'http://myHost.internal.com', :port => 9090
       #
       # Returns a ManageEngine::AppManager::Instance.
+      #
       def new(args = nil)
         ManageEngine::AppManager::Server.new args
       end
