a528d6b800144f46e2ec1be111323496b6d906bc
Justin Karimi <jekhokie@gmail.com>
2013-06-19 15:04:58 -0500

    Update Version11 Class for hosts and host_path
    Update the Version11 class to include the hosts method and host_path.

--- lib/manage_engine/app_manager/api/version11.rb
+++ lib/manage_engine/app_manager/api/version11.rb
@@ -4,15 +4,42 @@ module ManageEngine
       class Version11
         attr_reader :connect_path
 
+        ###########################
+        # URL Response Methods
+        ###########################
+
         def connect_path
           "/AppManager/xml/ListDashboards?apikey=%{api_key}"
         end
+
+        def host_path
+          "/AppManager/xml/ListServer?apikey=%{api_key}&type=%{type}"
+        end
+
+        ###########################
+        # Processing Methods
+        ###########################
 
         def connect_response_valid?(response)
           doc = Nokogiri::XML response
           status = doc.xpath("/AppManager-response/result/response")
 
           return (!status[0].nil? and status[0].attr("response-code") == "4000")
+        end
+
+        def hosts(response)
+          host_list = Hash.new
+          doc       = Nokogiri::XML response
+
+          doc.xpath("/AppManager-response/result/response/Server").each do |server_xml|
+            (host_list[server_xml.attr("Name")] = Array.new).tap do |server_monitors|
+              server_xml.xpath("./Service").each_with_index do |service_xml, i|
+                server_monitors.push service_xml.attr("TYPE")
+              end
+            end
+          end
+
+          host_list
         end
       end
     end
