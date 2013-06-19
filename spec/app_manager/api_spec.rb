b0cc99dd9b9c84eab40b33a184da6fd3d3e2f580
Justin Karimi <jekhokie@gmail.com>
2013-06-19 13:02:52 -0500

    Add Spec for Unknown API Version
    Update the API spec to include testing for an unknown version.

--- spec/app_manager/api_spec.rb
+++ spec/app_manager/api_spec.rb
@@ -8,8 +8,12 @@ describe ManageEngine::AppManager::Api do
   end
 
   describe "new" do
-    it "should return a new Api instance" do
+    it "returns a new Api instance" do
       ManageEngine::AppManager::Api.new("11").should be_instance_of(ManageEngine::AppManager::Api::Version11)
+    end
+
+    it "raises an error for an unknown version" do
+      expect{ ManageEngine::AppManager::Api.new("X") }.to raise_error
     end
   end
 end
