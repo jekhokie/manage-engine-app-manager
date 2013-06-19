38a055a31a4641d30c6c64b83481a3392a347916
Justin Karimi <jekhokie@gmail.com>
2013-06-19 15:04:25 -0500

    Add hosts Spec for Api
    Update the Api spec to include tests for the hosts method.

--- spec/app_manager/api/version11_spec.rb
+++ spec/app_manager/api/version11_spec.rb
@@ -1,30 +1,48 @@
 require 'spec_helper'
 
 describe ManageEngine::AppManager::Api::Version11 do
-  subject { ManageEngine::AppManager::Api::Version11 }
+  context "url response methods" do
+    describe "connect_path" do
+      it "returns the connect_path for AppManager" do
+        FactoryGirl.build(:api, :version => "11").connect_path.should be_instance_of(String)
+      end
+    end
 
-  describe "connect_path" do
-    it "returns the connect_path for AppManager" do
-      FactoryGirl.build(:api, :version => "11").connect_path.should be_instance_of(String)
+    describe "host_path" do
+      it "returns the host_path for AppManager" do
+        FactoryGirl.build(:api, :version => "11").host_path.should be_instance_of(String)
+      end
     end
   end
 
-  describe "connect_response_valid?" do
-    let(:api)              { FactoryGirl.build :api, :version => "11" }
-    let(:valid_response)   { File.open(File.dirname(__FILE__) + "/../../fixtures/valid_connect.xml", "r").read }
-    let(:invalid_response) { File.open(File.dirname(__FILE__) + "/../../fixtures/invalid_connect.xml", "r").read }
-    let(:non_xml)          { "ABC123" }
+  context "processing methods" do
+    describe "connect_response_valid?" do
+      let(:api)              { FactoryGirl.build :api, :version => "11" }
+      let(:valid_response)   { File.open(File.dirname(__FILE__) + "/../../fixtures/valid_connect.xml", "r").read }
+      let(:invalid_response) { File.open(File.dirname(__FILE__) + "/../../fixtures/invalid_connect.xml", "r").read }
+      let(:non_xml)          { "ABC123" }
 
-    it "returns true for a valid response" do
-      api.connect_response_valid?(valid_response).should == true
+      it "returns true for a valid response" do
+        api.connect_response_valid?(valid_response).should == true
+      end
+
+      it "returns false for an invalid response" do
+        api.connect_response_valid?(invalid_response).should == false
+      end
+
+      it "returns false for non-found response" do
+        api.connect_response_valid?(non_xml).should == false
+      end
     end
 
-    it "returns false for an invalid response" do
-      api.connect_response_valid?(invalid_response).should == false
-    end
+    describe "hosts" do
+      let(:api)       { FactoryGirl.build :api, :version => "11" }
+      let(:all_hosts) { File.open(File.dirname(__FILE__) + "/../../fixtures/all_hosts.xml", "r").read }
 
-    it "returns false for non-found response" do
-      api.connect_response_valid?(non_xml).should == false
+      it "returns a hash of service arrays" do
+        api.hosts(all_hosts).should == { "test-vm.local.host"  => [ "Service Monitoring", "Tomcat", "Tomcat", "JBoss", "PostgreSQL" ],
+                                         "test2-vm.local.host" => [ "Service Monitoring", "Tomcat", "PostgreSQL" ] }
+      end
     end
   end
 end
